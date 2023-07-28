4258bdb6dcdd9289eb41bc369ad57210f5e544de
virgil.balica <virgil.balica@tesco.com>
2023-07-28 13:32:13 +0300

    add gradle and a simple unit test

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println("Running Test");
+        assertEquals("true", "true");
+    }
+
+}
